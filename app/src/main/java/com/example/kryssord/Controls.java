b2d932fb7eb1402537dbe792131a42e715aeb751
mathiasck <mathias.ck2000@gmail.com>
2023-08-28 10:59:19 +0200

    Refactor MainActivity.java

---
+++ app/src/main/java/com/example/kryssord/Controls.java
@@ -0,0 +1,58 @@
+package com.example.kryssord;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Controls {
+    public static void deleteText() {
+        String currentText = MainActivity.text.getText().toString();
+    
+        if (!currentText.isEmpty()) {
+            String updatedText = currentText.substring(0, currentText.length() - 1);
+            MainActivity.text.setText(updatedText);
+        }
+    }
+    
+    public static void generateHint() {
+        Random generator = new Random();
+        int randomIndex;
+    
+        do {
+            randomIndex = generator.nextInt(MainActivity.correctWords.length);
+        } while (MainActivity.usedWords.contains(MainActivity.correctWords[randomIndex]));
+    
+        String hint = MainActivity.correctWords[randomIndex];
+        MainActivity.toast.setText(hint.substring(0, hint.length() - 1) + "*");
+    }
+    
+    public static void check() {
+        String inputText = MainActivity.text.getText().toString();
+        MainActivity.toast.setText("");
+    
+        if (inputText.length() < MainActivity.difficulty || !inputText.contains("A")) {
+            String toastMessage = "Word must contain " + MainActivity.difficulty + " letters and the letter A";
+            MainActivity.toast.setText(toastMessage);
+            return;
+        }
+    
+        if (MainActivity.usedWords.contains(inputText)) {
+            MainActivity.toast.setText("Word is already used");
+            return;
+        }
+    
+        if (!Arrays.asList(MainActivity.correctWords).contains(inputText)) {
+            MainActivity.toast.setText("Wrong word");
+            return;
+        }
+    
+        MainActivity.toast.setText("Correct!");
+        MainActivity.usedWords.add(inputText);
+    
+        int used = MainActivity.usedWords.size();
+        int correct = MainActivity.correctWords.length;
+        MainActivity.progress.setText(used + "/" + correct);
+        MainActivity.numCompleted.append(inputText + " ");
+    
+        MainActivity.text.setText("");
+    }
+}
--- app/src/main/java/com/example/kryssord/MainActivity.java
+++ app/src/main/java/com/example/kryssord/MainActivity.java
@@ -9,58 +9,16 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-    TextView text;
-    TextView toast;
-    TextView progress;
-    TextView answerBox;
-    TextView numCompleted;
-    int difficulty;
-    String [] letters;
-    String [] correctWords;
-    
-    @Override
-    protected void onSaveInstanceState(Bundle outstate) {
-        super.onSaveInstanceState(outstate);
-        
-        TextView saveText = (TextView) text;
-        outstate.putString("text",text.getText().toString());
-        
-        TextView saveToast = (TextView) toast;
-        outstate.putString("toast",toast.getText().toString());
-        
-        TextView saveProgress = (TextView) progress;
-        outstate.putString("progress",progress.getText().toString());
-        
-        TextView saveAnswer = (TextView) answerBox;
-        outstate.putString("answerBox",answerBox.getText().toString());
-        
-        TextView saveNumCompleted = (TextView) numCompleted;
-        outstate.putString("numCompleted",numCompleted.getText().toString());
-    }
-    
-    @Override
-    protected void onRestoreInstanceState(Bundle savedInstanceState) {
-        super.onRestoreInstanceState(savedInstanceState);
-        
-        TextView saveText = (TextView) text;
-        saveText.setText(savedInstanceState.getString("text"));
-        
-        TextView saveToast = (TextView) toast;
-        saveToast.setText(savedInstanceState.getString("toast"));
-        
-        TextView saveProgress = (TextView) progress;
-        saveProgress.setText(savedInstanceState.getString("progress"));
-        
-        TextView saveAnswer = (TextView) answerBox;
-        saveAnswer.setText(savedInstanceState.getString("answer"));
-        
-        TextView saveUsed = (TextView) numCompleted;
-        saveUsed.setText(savedInstanceState.getString("used"));
-    }
+    static TextView text;
+    static TextView toast;
+    static TextView progress;
+    static TextView numCompleted;
+    static int difficulty;
+    static String [] letters;
+    static String [] correctWords;
+    static ArrayList<String> usedWords = new ArrayList<>();
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -85,8 +43,8 @@ public class MainActivity extends AppCompatActivity {
         Resources res = getResources();
         letters = res.getStringArray(R.array.letters);
         
-        setupDifficultyButton(R.id.button_3, R.array.words_3_letters, 3);
-        setupDifficultyButton(R.id.button_4, R.array.words_4_letters, 4);
+        setupDifficultyButton(R.id.button_3_letters, R.array.words_3_letters, 3);
+        setupDifficultyButton(R.id.button_4_letters, R.array.words_4_letters, 4);
     }
     
     private void setupDifficultyButton(int buttonId, int wordsArrayId, final int difficultyLevel) {
@@ -101,165 +59,68 @@ public class MainActivity extends AppCompatActivity {
         });
     }
     
-    private void startGame() {
+    private void initButtons() {
+        int[] buttonIds = { R.id.button_A, R.id.button_1, R.id.button_2, R.id.button_3, R.id.button_4, R.id.button_5, R.id.button_6 };
+        Button[] buttons = new Button[buttonIds.length];
     
-        setContentView(R.layout.activity_main);
-        
-        ArrayList <String> usedWords = new ArrayList<>();
+        for (int i = 0; i < buttonIds.length; i++) {
+            buttons[i] = setupButtonClick(buttonIds[i], i);
+        }
+    }
     
-        Button button_left_bottom = (Button)findViewById(R.id.button_left_bottom);
-        Button button_left_top = (Button)findViewById(R.id.button_left_top);
-        Button button_middle = (Button)findViewById(R.id.button_middle);
-        Button button_top = (Button)findViewById(R.id.button_top);
-        Button button_bottom = (Button)findViewById(R.id.button_bottom);
-        Button button_right_bottom = (Button)findViewById(R.id.button_right_bottom);
-        Button button_right_top = (Button)findViewById(R.id.button_right_top);
+    private Button setupButtonClick(int buttonId, int i) {
+        Button button = findViewById(buttonId);
+    
+        button.setText(letters[i]);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                toast.setText("");
+                text.append(button.getText());
+            }
+        });
+        return button;
+    }
+    
+    private void initControls() {
         Button button_delete = (Button)findViewById(R.id.button_delete);
-        Button button_answer = (Button)findViewById(R.id.button_answer);
         Button button_check = (Button)findViewById(R.id.button_check);
         Button button_hint = (Button)findViewById(R.id.button_hint);
-    
-        text = (TextView) findViewById(R.id.txt_answer);
-        toast = (TextView) findViewById(R.id.error);
-        answerBox = (TextView) findViewById(R.id.answerBox);
-        numCompleted = (TextView) findViewById(R.id.numCompleted);
-        progress = (TextView) findViewById(R.id.progress);
-    
-        progress.setText(usedWords.size() +"/"+correctWords.length);
-    
-        Button[] buttons = { button_middle, button_left_bottom, button_left_top, button_top, button_bottom, button_right_bottom, button_right_top };
-    
-        for (int i = 0; i < buttons.length; i++) {
-            buttons[i].setText(letters[i]);
-        }
-    
-        button_left_bottom.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                toast.setText("");
-                text.append(button_left_bottom.getText());
-            }
-        });
-    
-        button_left_top.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                toast.setText("");
-                text.append(button_left_top.getText());
-            }
-        });
-    
-        button_middle.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                toast.setText("");
-                text.append(button_middle.getText());
-            }
-        });
-    
-        button_top.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                toast.setText("");
-                text.append(button_top.getText());
-            }
-        });
-    
-        button_bottom.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                toast.setText("");
-                text.append(button_bottom.getText());
-            }
-        });
-    
-        button_right_bottom.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                toast.setText("");
-                text.append(button_right_bottom.getText());
-            }
-        });
-    
-        button_right_top.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                toast.setText("");
-                text.append(button_right_top.getText());
-            }
-        });
     
         button_delete.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String currentText = text.getText().toString();
-    
-                if (!currentText.isEmpty()) {
-                    String updatedText = currentText.substring(0, currentText.length() - 1);
-                    text.setText(updatedText);
-                }
+                Controls.deleteText();
             }
         });
     
         button_check.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String inputText = text.getText().toString();
-                toast.setText("");
-                
-                if (inputText.length() < difficulty || !inputText.contains("A")) {
-                    String toastMessage = "Word must contain " + difficulty + " letters and the letter A";
-                    toast.setText(toastMessage);
-                    return;
-                }
-                
-                if (usedWords.contains(inputText)) {
-                    toast.setText("Word is already used");
-                    return;
-                }
-                
-                if (!Arrays.asList(correctWords).contains(inputText)) {
-                    toast.setText("Wrong word");
-                    return;
-                }
-                
-                toast.setText("Correct!");
-                usedWords.add(inputText);
-    
-                int used = usedWords.size();
-                int correct = correctWords.length;
-                progress.setText(used + "/" + correct);
-                numCompleted.append(inputText + " ");
-    
-                text.setText("");
+                Controls.check();
             }
         });
-    
         button_hint.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-    
-                Random generator = new Random();
-                int randomIndex;
-                
-                do {
-                    randomIndex = generator.nextInt(correctWords.length);
-                } while (usedWords.contains(correctWords[randomIndex]));
-    
-                String hint = correctWords[randomIndex];
-                toast.setText(hint.substring(0, hint.length() - 1) + "*");
+                Controls.generateHint();
             }
         });
+    }
+    private void startGame() {
     
-        button_answer.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                answerBox.setText("");
-                
-                for (String i : correctWords) {
-                    answerBox.append(i + ",");
-                }
-            }
-        });
+        setContentView(R.layout.activity_main);
+    
+        text = (TextView) findViewById(R.id.txt_letters);
+        toast = (TextView) findViewById(R.id.txt_toast);
+        numCompleted = (TextView) findViewById(R.id.txt_completed);
+        progress = (TextView) findViewById(R.id.txt_progress);
+    
+        progress.setText(usedWords.size() + "/" + correctWords.length);
+        toast.setText("Select " + difficulty + " letters");
+    
+        initButtons();
+        initControls();
+    
     }
 }
